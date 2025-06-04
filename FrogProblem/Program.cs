4b764b8e15aaa1cef514d6532b8c494ddc95843f
Dhanudharsan06 <dhanudharsan6374@gmail.com>
2025-06-04 21:15:37 +0530

    Updated version 1.0

--- FrogProblem/Program.cs
+++ FrogProblem/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace AlgoProblem;
+﻿namespace FrogProblem;
 public class Program
 {
     public static int Solution(int startPosition, int stopPosition, int jumpDistance)
@@ -14,30 +14,41 @@ public class Program
     public static int getNum(string sentence)
     {
         int num;
+
         Console.WriteLine($"Enter the valid value for {sentence}");
-        if (int.TryParse(Console.ReadLine(), out num) && num >= 0) { }
+
+        if (int.TryParse(Console.ReadLine(), out num) && num >= 0) 
+        { 
+        }
         else
         {
 
             num = getNum(sentence);
         }
+
         return num;
     }
     public static void Main(string[] args)
     {
         int start = getNum("Starting point");
         int end = -1;
+        int jump = 0;
+
         while (end < start)
         {
             Console.WriteLine("Enter Destination greater than or equal to Starting Point");
             end = getNum("Ending point");
         }
-        int jump = 0;
+
         while (jump <= 0)
         {
             jump = getNum("Jump distance");
         }
-        Console.WriteLine($"\n \n \nThe frog need to jump {Solution(start, end, jump)} times to reach the destination.");
+
+        int jumpCount= Solution(start, end, jump);
+
+        Console.WriteLine($"\n \n \nThe frog need to jump {jumpCount} times to reach the destination.");
         Console.ReadLine();
+ 
     }
 }
